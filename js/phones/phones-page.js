3b46ea3a8e9a4564f6ca8a6d3bd185fae0e0a78f
mgrinko <glukym@gmail.com>
2019-01-31 19:22:58 +0200

    Component events were planned

--- js/phones/phones-page.js
+++ js/phones/phones-page.js
@@ -30,11 +30,24 @@ export default class PhonesPage {
       this._catalog.hide();
       this._viewer.show(phoneDetails);
     });
+
+    this._catalog.subscribe('phone-added', (phoneId) => {
+      this._cart.add(phoneId);
+    });
   }
 
   _initViewer() {
     this._viewer = new PhoneViewer({
       element: this._element.querySelector('[data-component="phone-viewer"]'),
+    });
+
+    this._viewer.subscribe('back', () => {
+      this._viewer.hide();
+      this._catalog.show();
+    });
+
+    this._viewer.subscribe('add', (phoneId) => {
+      this._cart.add(phoneId);
     });
   }
 

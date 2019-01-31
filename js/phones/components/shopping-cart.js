16f8e0f3c7d3f9aeb31e150dc9d546d980bdd7de
mgrinko <glukym@gmail.com>
2019-01-31 20:04:40 +0200

    ShoppingCart remove was implemented

--- js/phones/components/shopping-cart.js
+++ js/phones/components/shopping-cart.js
@@ -1,14 +1,29 @@
-export default class ShoppingCart {
+import Component from '../../component.js';
+
+export default class ShoppingCart extends Component {
   constructor({ element }) {
-    this._element = element;
+    super({ element });
 
     this.items = [];
 
     this._render();
+
+    this.on('click', 'remove-button', (event) => {
+      let button = event.target;
+
+      this.remove(button.dataset.itemId);
+    });
   }
 
   add(itemId) {
     this.items.push(itemId);
+
+    this._render();
+  }
+
+  remove(itemIdToRemove) {
+    this.items = this.items
+      .filter(itemId => itemId !== itemIdToRemove);
 
     this._render();
   }
@@ -20,6 +35,13 @@ export default class ShoppingCart {
         ${ this.items.map(itemId => `
           <li>
             ${ itemId }
+            
+            <button
+              data-element="remove-button"
+              data-item-id="${ itemId }"
+            >
+              X
+            </button>
           </li>
         `).join('')}
       </ul>
